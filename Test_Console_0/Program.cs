9867a88343d3c28fe3277f3866a172ff5f3242ad
Nelson Sandoval Puente <nelsonsandovalpuente@tutanota.com>
2020-08-27 08:45:32 -0400

    Code of formulas tested by hand.

---
+++ Test_Console_0/Program.cs
@@ -0,0 +1,29 @@
+﻿using Solver_missile_0;
+using System;
+using System.Collections.Generic;
+
+namespace Test_Console_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Solver_missile_0.Projectile projectile = new Solver_missile_0.Projectile();
+            projectile.F_0 = new List<double> { 1, 1, 1 };
+            projectile.X_0= new List<double> { 2, 2, 2 };
+            projectile.V_0 = new List<double> { 3, 3, 3 };
+            projectile.M = 1;
+            projectile.T_0 = 0;
+            projectile.Initialize();
+            projectile.Calc_X(5);
+            projectile.Calc_V(5);
+            Console.WriteLine($"x={projectile.X[2]}");
+            Console.WriteLine($"v={projectile.V[2]}");
+            for (int i=0; i<projectile.X.Count; i++)
+            {
+                Console.WriteLine($"{i}, {projectile.X[i]}, {projectile.V[i]}");
+            }
+        }
+    }
+}
--- solver_missile_0/Projectile.cs
+++ solver_missile_0/Projectile.cs
@@ -2,34 +2,75 @@
 using System.Collections.Generic;
 using System.Net.Http.Headers;
 
-namespace solver_missile_0
+namespace Solver_missile_0
 {
+    public static class Physics
+    {
+        public static double Calc_X_Projectile_0(double t, double x_0, double v_0, double F_0, double m, double t_0)
+        {
+            return F_0 / (2 * m) * Math.Pow(t - t_0, 2) + v_0 * (t - t_0) + x_0;
+        }
+        public static double Calc_V_Projectile_0(double t, double v_0, double F_0, double m, double t_0)
+        {
+            return F_0 / m * (t - t_0) + v_0;
+        }
+    }
     public class Projectile
     {
+        public Projectile(List<double> x_0, List<double> v_0, List<double> f_0, double m, double t_0)
+        {
+            X_0 = new List<double>(x_0);
+            V_0 = new List<double>(v_0);
+            F_0 = new List<double>(f_0);
+            M = m;
+            T_0 = t_0;
+            x = new List<double>(X_0);
+            v = new List<double>(V_0);
+        }
+        public Projectile()
+        {
+            X_0 = new List<double> { 0, 0, 0};
+            V_0 = new List<double> { 0, 0, 0 };
+            F_0 = new List<double> { 0, 0, 0 };
+            M = 0;
+            T_0 = 0;
+            x = new List<double> { 0, 0, 0 };
+            v = new List<double> { 0, 0, 0 };
+        }
+
+        private List<double> x;
+        public List<double> X { get => x; }
+        public void Calc_X(double t)
+        {
+            List<double> retval = new List<double> { 0, 0, 0};
+            
+            for (int i = 0; i < retval.Count; i++)
+            {
+                
+                retval[i] = Physics.Calc_X_Projectile_0(t, X_0[i], V_0[i], M, F_0[i], T_0);
+            }
+            x = retval;
+        }
+        private List<double> v;
+        public List<double> V { get => v; }
+        public void Calc_V(double t)
+        {
+            List<double> retval = new List<double> { 0, 0, 0};
+            for (int i = 0; i < retval.Count; i++)
+            {
+                retval[i] = Physics.Calc_V_Projectile_0(t, V_0[i], F_0[i], M, T_0);
+            }
+            v=retval;
+        }
         public List<double> X_0 { get; set; }
         public List<double> V_0 { get; set; }
         public List<double> F_0 { get; set; }
-        public List<double> X { get; }
-        public List<double> V { get; set; }
         public double M { get; set; }
         public double T_0 { get; set; }
-        public List<double> Calc_X(double t)
+        public void Initialize()
         {
-            List<double> retval = new List<double>(3);
-            for (int i = 0; i < retval.Count; i++)
-            {
-                retval[i] = F_0[i] / (2 * M) * Math.Pow(t - T_0, 2) + V_0[i]*(t - T_0) + X_0[i];
-            }
-            return retval;
-        }
-        public List<double> Calc_V(double t)
-        {
-            List<double> retval = new List<double>(3);
-            for (int i = 0; i < retval.Count; i++)
-            {
-                retval[i] = F_0[i] / M * (t - T_0) + V_0[i];
-            }
-            return retval;
+            x = X_0;
+            v = V_0;
         }
     }
 }
